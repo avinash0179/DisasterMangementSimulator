eb145098617467a0cadcdd2e068c5eb1e7d3d1cb
avinash0179 <avinash.pedada1704@gmail.com>
2026-03-09 08:58:20 +0530

    updated code

--- script.js
+++ script.js
@@ -1,29 +1,183 @@
-let victims=[];
+// Victim Class
+class Victim {
+
+constructor(id){
+this.id=id;
+
+this.healthStatus=Math.random()>0.5?"Severe":"Stable";
+
+this.priority=this.healthStatus==="Severe"?"Critical":"Normal";
+
+this.status="Waiting";
+}
+
+requestHelp(){
+return "Victim "+this.id+" requesting help";
+}
+
+updateCondition(){
+if(this.healthStatus==="Severe"){
+this.priority="Critical";
+}
+}
+
+}
+
+
+// Disaster Class
+class Disaster{
+
+constructor(type,severity,location){
+
+this.type=type;
+this.severity=severity;
+this.location=location;
+
+}
+
+start(){
+
+console.log(this.type+" disaster started at "+this.location);
+
+}
+
+update(){
+
+console.log("Disaster updated");
+
+}
+
+}
+
+
+// Rescue Team Class
+class RescueTeam{
+
+constructor(teamType,capacity){
+
+this.teamType=teamType;
+
+this.capacity=capacity;
+
+}
+
+move(){
+
+console.log(this.teamType+" moving to disaster area");
+
+}
+
+rescueVictim(victim){
+
+if(this.capacity>0 && victim.status==="Waiting"){
+
+victim.status="Rescued";
+
+this.capacity--;
+
+}
+
+}
+
+}
+
+
+// Resource Class
+class Resource{
+
+constructor(type,quantity){
+
+this.type=type;
+
+this.quantity=quantity;
+
+}
+
+allocate(){
+
+if(this.quantity>0){
+this.quantity--;
+}
+
+}
+
+release(){
+
+this.quantity++;
+
+}
+
+}
+
+
+// Simulator Class
+class Simulator{
+
+constructor(){
+
+this.victims=[];
+
+this.rescueTeam=new RescueTeam("Emergency Team",3);
+
+}
+
+runSimulation(disasterType){
+
+let disaster=new Disaster(disasterType,"High","City");
+
+disaster.start();
+
+this.victims=[];
+
+for(let i=1;i<=5;i++){
+
+this.victims.push(new Victim(i));
+
+}
+
+}
+
+rescueProcess(){
+
+this.victims.forEach(v=>{
+
+this.rescueTeam.rescueVictim(v);
+
+});
+
+}
+
+generateReport(){
+
+let rescued=this.victims.filter(v=>v.status==="Rescued").length;
+
+return{
+total:this.victims.length,
+rescued:rescued
+};
+
+}
+
+}
+
+
+// Global simulator object
+let simulator=new Simulator();
+
+
+// UI FUNCTIONS
+
 
 function startSimulation(){
 
 let disaster=document.getElementById("disasterType").value;
 
-alert("Simulation started for "+disaster);
-
-victims=[];
-
-for(let i=1;i<=6;i++){
-
-let priority=Math.random()>0.5?"Critical":"Normal";
-
-victims.push({
-id:i,
-health:priority==="Critical"?"Severe":"Stable",
-priority:priority,
-status:"Waiting"
-});
-
-}
+simulator.runSimulation(disaster);
 
 displayVictims();
 
 }
+
 
 function displayVictims(){
 
@@ -31,16 +185,16 @@ let table=document.querySelector("#victimTable tbody");
 
 table.innerHTML="";
 
-victims.forEach(v=>{
+simulator.victims.forEach(v=>{
 
-let row=`
-<tr>
+let row=
+
+`<tr>
 <td>${v.id}</td>
-<td>${v.health}</td>
+<td>${v.healthStatus}</td>
 <td>${v.priority}</td>
 <td>${v.status}</td>
-</tr>
-`;
+</tr>`;
 
 table.innerHTML+=row;
 
@@ -48,28 +202,23 @@ table.innerHTML+=row;
 
 }
 
+
 function assignTeam(){
 
-for(let v of victims){
-
-if(v.status==="Waiting"){
-v.status="Rescued";
-break;
-}
-
-}
+simulator.rescueProcess();
 
 displayVictims();
 
 }
 
+
 function generateReport(){
 
-let rescued=victims.filter(v=>v.status==="Rescued").length;
-
-let total=victims.length;
+let report=simulator.generateReport();
 
 document.getElementById("report").innerHTML=
-`Total Victims: ${total}<br>Rescued Victims: ${rescued}`;
+
+"Total Victims: "+report.total+
+"<br>Rescued Victims: "+report.rescued;
 
 }
